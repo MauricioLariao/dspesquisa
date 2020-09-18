c662943ab8f1f4c92e64b4b0c9e77e6e6c5a9798
mauriciolariao <mauricio.lariao@gmail.com>
2020-09-18 00:35:11 -0300

    Criando navegação com rotas

---
+++ front-web1/src/Routes.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+//instala a extensao react-router-dom pelo npm install react-router-dom para tratar as rotas do app
+//e instala as dependencias do typescript instalando npm install @types/react-router-dom --dev
+import { BrowserRouter, Switch, Route } from "react-router-dom";  //importar o switch e router para casar as rotas daaplicaca
+import Header from './components/header';
+import Home from './pages/Home';
+import Records from './pages/Records';
+
+//quando digitar / no browser ja cai direto na pagina home
+const Routes = () => (
+    <BrowserRouter>
+        <Header />
+        <Switch>
+            <Route path='/' exact> 
+                <Home />
+            </Route>
+            <Route path='/records'> 
+                <Records />
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
+
